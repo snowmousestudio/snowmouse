c50a264d281d1060a738beb0a35f2605ec444965
rajbirjohar <rajbirjohar1@gmail.com>
2023-06-20 12:04:52 -0700

    Update footer and layout styles

--- core/common/Footer/index.tsx
+++ core/common/Footer/index.tsx
@@ -6,23 +6,23 @@ export default function Footer() {
     <footer className={styles.footer}>
       <div className={styles.content}>
         <h3 className={styles.logo}>Snow Mouse Studio</h3>
-        <div className={styles.column}>
-          <h5>Resources</h5>
-          <a>Code</a>
-          <a>Journals</a>
-        </div>
-        <div className={styles.column}>
-          <h5>Navigate</h5>
-          <a>Home</a>
-          <a>Portfolio</a>
-          <a>About</a>
-        </div>
-        <div className={styles.column}>
-          <h5>Elsewhere</h5>
-          <a>Twitter</a>
-          <a>Github</a>
-          <a>JS</a>
-          <a>Rajbir</a>
+        <div className={styles.links}>
+          <div className={styles.column}>
+            <h5>Resources</h5>
+            <a>Twitter</a>
+            <a>Github</a>
+          </div>
+          <div className={styles.column}>
+            <h5>Navigate</h5>
+            <a>Home</a>
+            <a>Portfolio</a>
+            <a>About</a>
+          </div>
+          <div className={styles.column}>
+            <h5>Us</h5>
+            <a>JS</a>
+            <a>Rajbir</a>
+          </div>
         </div>
       </div>
       <div className={styles.copyright}>
